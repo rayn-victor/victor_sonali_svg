d52c73b1dcf9e6b17316583c23fb26909584a74b
rayn victor <victor.impala67@gmail.com>
2019-01-26 20:52:41 -0500

    corrected mistake

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
 	allTheIcons.forEach(currenticon => currenticon.addEventListener('click', logIcon));
 	var allTheIcons = document.querySelectorAll('.icon2');
 	function logIcon() {
-		console.log("This is the icon for", this.id, "with the ID=", this.alt, "from the icon pack Retro Pack");
+		console.log("This is the icon for", this.id, "with the ID=", this.alt, "from the icon pack Minimalist Black");
 	}
 	icon.addEventListener("click", logIcon);
 	allTheIcons.forEach(currenticon => currenticon.addEventListener('click', logIcon));
